2d97c06012ac5e3b8507af3a1ba78a5991e33f77
Anthony J. Clark <ajc@>
2019-04-10 12:52:09 -0500

    Cleaned up code and xml files.

--- setup_env.bash
+++ setup_env.bash
@@ -1,10 +1,10 @@
-# 
+#
 # Source this file to export necessary environment variables
 # run with
 #    . setup_env.bash
-# 
+#
 
-# This path is temporary
+# Path to the controller plugin library (must be built first)
 export GAZEBO_PLUGIN_PATH=$(pwd)/controller/build:$GAZEBO_PLUGIN_PATH
 
 # All models are in the models directory
@@ -13,6 +13,6 @@ export GAZEBO_MODEL_PATH=$(pwd)/models:$GAZEBO_MODEL_PATH
 # The following export seems to break Gazebo
 # export GAZEBO_RESOURCE_PATH=$(pwd)/models:$GAZEBO_RESOURCE_PATH
 
-echo GAZEBO_PLUGIN_PATH is $GAZEBO_PLUGIN_PATH
-echo GAZEBO_MODEL_PATH is $GAZEBO_MODEL_PATH
-echo GAZEBO_RESOURCE_PATH is $GAZEBO_RESOURCE_PATH
+echo "GAZEBO_PLUGIN_PATH   " is $GAZEBO_PLUGIN_PATH
+echo "GAZEBO_MODEL_PATH    " is $GAZEBO_MODEL_PATH
+echo "GAZEBO_RESOURCE_PATH " is $GAZEBO_RESOURCE_PATH
