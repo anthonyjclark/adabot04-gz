982bde0211cf8ac044c7c82523c6e6237b09595b
Anthony J. Clark <anthonyclark@gmail.com>
2019-04-12 09:21:18 -0500

    Added check for env variables in run command.

--- run.sh
+++ run.sh
@@ -1,7 +1,14 @@
 #!/usr/bin/env bash
 
-if [[ "$1" == "heightmap" ]]; then
-    gazebo --pause --verbose worlds/test_heightmap.world
+# Setup environment
+if [[ ":$GAZEBO_MODEL_PATH" == *":$(pwd)"* ]]; then
+
+    if [[ "$1" == "heightmap" ]]; then
+        gazebo --pause --verbose worlds/test_heightmap.world
+    else
+        gazebo --pause --verbose worlds/empty.world
+    fi
+
 else
-    gazebo --pause --verbose worlds/empty.world
+    echo "You need to source setup_env.sh"
 fi
