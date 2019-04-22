419d917a1312017d9d684535c3fa758b8bc2431c
Anthony J. Clark <ajc@>
2019-04-21 21:09:27 -0500

    Updated env script with new plugin directory.

--- setup_env.sh
+++ setup_env.sh
@@ -5,7 +5,7 @@
 #
 
 # Path to the controller plugin library (must be built first)
-export GAZEBO_PLUGIN_PATH=$(pwd)/controller/build:$GAZEBO_PLUGIN_PATH
+export GAZEBO_PLUGIN_PATH=$(pwd)/plugins/keyboard_controller/build:$GAZEBO_PLUGIN_PATH
 
 # All models are in the models directory
 export GAZEBO_MODEL_PATH=$(pwd)/models:$GAZEBO_MODEL_PATH
